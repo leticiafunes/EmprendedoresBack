5c50011e8c3e408f3ecd7b6c58eec66343a3f7ba
Leticia Funes <letyfunes@gmail.com>
2022-01-31 00:07:39 -0300

    readme

--- src/controllers/emprendedores.controller.js
+++ src/controllers/emprendedores.controller.js
@@ -1,77 +1,86 @@
-const emprendedorCtrl = {}
+const emprendedorCtrl = {};
 
-const Emprendedor = require ('../models/Emprendedor');
+const Emprendedor = require("../models/Emprendedor");
 
+emprendedorCtrl.getEmprendedores = async (req, res) => {
+  const emprendedores = await Emprendedor.find();
+  res.json(emprendedores);
+};
 
+emprendedorCtrl.createEmprendedor = async (req, res) => {
+  const {
+    nombre,
+    activo,
+    tags,
+    resena,
+    telefono,
+    mail,
+    redes,
+    imagen,
+    nombre_emprendimiento,
+    rubro,
+    descripcioncorta,
+  } = req.body;
 
+  const newEmprendedor = new Emprendedor({
+    nombre,
+    activo,
+    tags,
+    resena,
+    telefono,
+    mail,
+    redes,
+    imagen,
+    nombre_emprendimiento,
+    rubro,
+    descripcioncorta,
+  });
 
-emprendedorCtrl.getEmprendedores =  async (req, res) => {
-    const emprendedores= await Emprendedor.find( );
-    res.json (emprendedores);
+  await newEmprendedor.save();
 
-}
+  res.json({ message: ["Crear emprendedor"] });
+};
+emprendedorCtrl.deleteEmprendedor = async (req, res) => {
+  await Emprendedor.findByIdAndDelete(req.params.id);
+  res.json({ message: ["Borrar Emprendedor"] });
+};
 
+emprendedorCtrl.updateEmprendedor = async (req, res) => {
+  const {
+    nombre,
+    activo,
+    tags,
+    resena,
+    telefono,
+    mail,
+    redes,
+    imagen,
+    nombre_emprendimiento,
+    rubro,
+    descripcioncorta,
+  } = req.body;
 
-emprendedorCtrl.createEmprendedor =  async (req, res) => {
+  const filter = { _id: req.params.id };
+  const respuesta = await Emprendedor.findOneAndUpdate(filter, {
+    nombre,
+    activo,
+    tags,
+    resena,
+    telefono,
+    mail,
+    redes,
+    imagen,
+    nombre_emprendimiento,
+    rubro,
+    descripcioncorta,
+  });
 
-    
+  res.json({ message: ["Emprendedor Actualizado"] });
+};
 
-    const {  nombre, activo, tags, resena, telefono, mail, redes, imagen, nombre_emprendimiento, rubro, descripcioncorta } = req.body;
-
-      const newEmprendedor= new Emprendedor ({
-        nombre, 
-        activo, 
-        tags, 
-        resena, 
-        telefono, 
-        mail, 
-        redes, 
-        imagen,
-        nombre_emprendimiento,
-        rubro,
-        descripcioncorta})
-        
-        await newEmprendedor.save();
-        
-
-
-    res.json ({message : ['Crear emprendedor']})
-
-}
-emprendedorCtrl.deleteEmprendedor =  
-        async (req, res) => {
-            await Emprendedor.findByIdAndDelete (req.params.id);
-            res.json ({message : ['Borrar Emprendedor']})
-
-        }
-
-        //Primer commit
-
-emprendedorCtrl.updateEmprendedor=  
-        async (req, res) => {
-            
-           
-            const {  nombre, activo, tags, resena, telefono, mail, redes, imagen, nombre_emprendimiento, rubro, descripcioncorta} = req.body;
-        
-            const filter =  { "_id" : req.params.id }
-            const respuesta = await Emprendedor.findOneAndUpdate (filter,  { nombre, activo, tags, resena, telefono, mail, redes, imagen,nombre_emprendimiento, rubro, descripcioncorta })
-            
-        
-            res.json ({message : ['Emprendedor Actualizado']})
-        }
-
-
-
-
-
-        
-
-
-
-emprendedorCtrl.getEmprendedor= 
-        async (req, res) => {
-        const emprendedor=  await Emprendedor.findById (req.params.id);
-        res.json (emprendedor)
-    } 
+emprendedorCtrl.getEmprendedor = async (req, res) => {
+  const emprendedor = await Emprendedor.findById(req.params.id);
+  res.json(emprendedor);
+};
 
 module.exports = emprendedorCtrl;
